b667508272df878286384589f41b35df1fa29313
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-04-23 13:08:19 +0300

    delete class bucketService

--- src/main/java/mate/academy/internetshop/service/BucketService.java
+++
@@ -1,8 +0,0 @@
-package mate.academy.internetshop.service;
-
-import mate.academy.internetshop.model.Bucket;
-import mate.academy.internetshop.model.Product;
-
-public interface BucketService {
-    Bucket addItem(Long bucketId, Product product);
-}
